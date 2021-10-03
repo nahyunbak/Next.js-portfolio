a1f842111f9c5eb62a4b3110d03a1f20d773537c
nahyunbak <nahyunbak@naver.com>
2021-10-03 11:31:25 +0900

    Create toggle login in skills

--- components/Skills/index..tsx
+++ components/Skills/index..tsx
@@ -3,24 +3,68 @@ import { SkillsWrapper } from "./StyledSkillts";
 
 const Skills = () => {
   const [skillData, setSkillData] = useState(["github", "deploy"]);
+  const [frontData, setFrontData] = useState([]);
+  const [backData, setBackData] = useState([]);
+  const frontSkills = ["github"];
+  const backSkills = ["deploy"];
   const removeImg = (skill) => {
-    const tempSkillData = skillData;
     setSkillData((oldState) => oldState.filter((item) => item !== skill));
+    console.log(skillData);
+    if (frontSkills.includes(skill)) {
+      setFrontData((oldState) => [...oldState, skill]);
+    }
+    if (backSkills.includes(skill)) {
+      setFrontData((oldState) => [...oldState, skill]);
+    }
+  };
+
+  const toggleFront = (skill) => {
+    setFrontData((oldState) => oldState.filter((item) => item !== skill));
+    setSkillData((oldState) => [...oldState, skill]);
+
+    console.log(skillData);
+  };
+
+  const toggleBack = (skill) => {
+    setBackData((oldState) => oldState.filter((item) => item !== skill));
+    setSkillData((oldState) => [...oldState, skill]);
+
     console.log(skillData);
   };
 
   return (
-    <SkillsWrapper>
-      {skillData.map((skill: string, index) => {
+    <>
+      <SkillsWrapper>
+        {skillData.map((skill: string, index) => {
+          return (
+            <img
+              key={index}
+              src={`/${skill}.png`}
+              onClick={() => removeImg(skill)}
+            />
+          );
+        })}
+
+        {backData.map((skill: string, index) => {
+          return (
+            <img
+              key={index}
+              src={`/${skill}.png`}
+              onClick={() => toggleBack(skill)}
+            />
+          );
+        })}
+      </SkillsWrapper>
+      {frontData.map((skill: string, index) => {
         return (
           <img
             key={index}
             src={`/${skill}.png`}
-            onClick={() => removeImg(skill)}
+            onClick={() => toggleFront(skill)}
           />
         );
       })}
-    </SkillsWrapper>
+    </>
   );
 };
 
